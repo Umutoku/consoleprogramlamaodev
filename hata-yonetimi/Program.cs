3fbbfee1fd1e37c8d680e5760de47eb94de94c2b
Umut Oku <umut.oku@gmail.com>
2022-01-22 02:22:17 +0300

    Hata yönetimi eklendi

---
+++ hata-yonetimi/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace hata_yonetimi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+            Console.WriteLine("Bir sayı giriniz");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Grimiş olduğunuz sayı: "+ sayi);
+            }
+            catch(ArgumentNullException ex)
+            {
+                Console.WriteLine("Hata: "+ ex.Message.ToString());
+            }
+            catch(FormatException fex)
+            {
+                Console.WriteLine("Hata: "+ fex.Message.ToString());
+            }
+
+            finally
+            {
+                Console.Write("İşlem Tamamlandı");
+            }
+
+        }
+    }
+}
