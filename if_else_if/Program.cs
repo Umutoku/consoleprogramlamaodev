17477e0514d534819a0e6e1df3c61b55ac09d9ce
Umut Oku <umut.oku@gmail.com>
2022-01-22 16:15:18 +0300

    if else eklendi

---
+++ if_else_if/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace if_else_if
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int time = DateTime.Now.Hour;
+
+            if (time <= 18)
+            {
+                Console.WriteLine("İyi günler");
+
+
+            }
+
+else if(time >=6 && time <11)
+{
+    Console.WriteLine("Günaydın");
+}
+
+
+
+            else
+
+
+
+            {
+                Console.WriteLine("İyi geceler");
+            }
+
+string sonuc = time <=18 ? "İyi Günler" : "İyi geceler";
+
+        sonuc = time >=6 && time <= 11 ? "İyi Günler" : time <=18 ? "İyi günler": "İyi geceler";
+        Console.WriteLine(sonuc);
+        }
+    }
+}
