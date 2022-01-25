f2741585a6f11d9e45a2baf605f0b09f82e22a8f
Umut Oku <umut.oku@gmail.com>
2022-01-25 15:43:02 +0300

    Array dizi  yapısı eklendi

---
+++ Diziler-array-sinif-metotlari/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace if_else_if
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        //Sort
+        int[] sayidizisi ={2,12,4,86,72,3,11,17};
+        Console.WriteLine("***Sırasız Dizi***");
+        foreach (var sayi in sayidizisi)
+       Console.WriteLine(sayi);
+
+       Console.WriteLine("*** Sıralı Dizi ***");
+       Array.Sort(sayidizisi);
+
+        foreach (var sayi in sayidizisi)
+       Console.WriteLine(sayi);
+
+
+        //Clear
+        Console.WriteLine("*** Array Clear ***");
+        //Sayı dizisi elemanlarını kullanarak ikinci indexten itibaren iki elemanı sıfırlar
+        Array.Clear(sayidizisi,2,2);
+
+            foreach (var sayi in sayidizisi)
+       Console.WriteLine(sayi);
+
+       //Reverse
+       Console.WriteLine("*** Array Reverse ***");
+       Array.Reverse(sayidizisi);
+
+         foreach (var sayi in sayidizisi)
+       Console.WriteLine(sayi);
+
+       //Indexof
+       Console.WriteLine("*** Array IndexOf ***");
+      Console.WriteLine(Array.IndexOf(sayidizisi,23));
+
+      
+
+      //Resize
+      Console.WriteLine("*** Array Resize ***");
+      Array.Resize int(ref sayidizisi,9));
+      sayidizisi[8] = 99;
+
+      foreach(var sayi in sayidizisi)
+      Console.WriteLine(sayi);
+
+                }
+    }
+}
